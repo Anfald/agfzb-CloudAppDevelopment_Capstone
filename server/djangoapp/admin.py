9071578a8f37b934b42a4169f54d54c48245ce1e
Anfald <46535230+Anfald@users.noreply.github.com>
2022-09-30 06:29:48 +0300

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,31 +1,39 @@
 from django.contrib import admin
 # from .models import related models
 from .models import CarMake, CarModel
+# if i want to import all models
+#from .models import *
 
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 5
 
-# Register your models here.
-admin.site.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    
+# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel)
-
-
+    
+# Register your models here.
+#admin.site.register(CarMake)
+#admin.site.register(CarModel)
 # CarModelInline class
-class CarModelInline:
-    pass
+#class CarModelInline:
+ #   pass
 
 
 # CarModelAdmin class
-class CarModelAdminn(admin.ModelAdmin):
-    list_display = ['title']
+#class CarModelAdminn(admin.ModelAdmin):
+ #   list_display = ['title']
     
     
 # CarMakeAdmin class with CarModelInline
 # here i will updated at the end
-class CarMakeAdmin(admin.ModelAdmin):
-    inlines = [CarModelInline]
-    list_display = ('name', 'pub_date')
-    list_filter = ['pub_date']
-    search_fields = ['name', 'description']
+#class CarMakeAdmin(admin.ModelAdmin):
+#   inlines = [CarModelInline]
+#   list_display = ('name', 'pub_date')
+#  list_filter = ['pub_date']
+# search_fields = ['name', 'description']
 
-# Register models here
-admin.site.register(CarMake, CarMakeAdmin)
-admin.site.register(CarModel, CarModelAdminn)
+
