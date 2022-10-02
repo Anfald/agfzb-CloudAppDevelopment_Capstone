99255e6f00c1b279395ffe9e6cc38cd4bd17c5a1
Anfald <46535230+Anfald@users.noreply.github.com>
2022-10-02 19:29:29 +0300

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -27,8 +27,8 @@ def about(request):
 # Create a `contact` view to return a static contact page
 def contact(request):
    context = {}
-    if request.method == 'GET':
-        return render(request, 'djangoapp/contact.html', context)
+   if request.method == 'GET':
+      return render(request, 'djangoapp/contact.html', context)
 
 # Create a `login_request` view to handle sign in request
 def login_request(request):
