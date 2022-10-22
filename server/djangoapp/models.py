ce8b54ae4784b1192ba089f643ef68a716833f23
Anfald <46535230+Anfald@users.noreply.github.com>
2022-10-22 17:20:34 +0300

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -62,30 +62,52 @@ class CarDealer:
 
     def __str__(self):
         return "Dealer name: " + self.full_name
-
-# <HINT> Create a plain Python class `DealerReview` to hold review data
+ # <HINT> Create a plain Python class `DealerReview` to hold review data
 class DealerReview:
-    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment):
-        # Dealership
+
+    def __init__(self, dealership, name, purchase, id, review, sentiment, **kwargs):
+        
         self.dealership = dealership
-        # Dealer name
         self.name = name
-        # Dealer purchase
         self.purchase = purchase
-        # Dealer review
+        self.id = id
         self.review = review
-        # purchase_date
-        self.purchase_date = purchase_date
-        # car_make
-        self.car_make = car_make
-        # car_model
-        self.car_model = car_model
-        # car_year
-        self.car_year = car_year
-        # sentiment
         self.sentiment = sentiment
+        
+        if purchase:
+            self.purchase_date = kwargs["purchase_date"]
+            self.car_make = kwargs["car_make"]
+            self.car_model = kwargs["car_model"]
+            self.car_year = kwargs["car_year"]
+        
+        
+    def __str__(self):
+        return "Review: " + self.review
+############
+# <HINT> Create a plain Python class `DealerReview` to hold review data
+#class DealerReview:
+ #   def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment):
+  #      # Dealership
+  #      self.dealership = dealership
+        # Dealer name
+  #     self.name = name
+        # Dealer purchase
+    #    self.purchase = purchase
+        # Dealer review
+     #   self.review = review
+        # purchase_date
+   #     self.purchase_date = purchase_date
+        # car_make
+   #     self.car_make = car_make
+        # car_model
+   #     self.car_model = car_model
+        # car_year
+   #     self.car_year = car_year
+        # sentiment
+   #     self.sentiment = sentiment
         # id
        # self.id = id
 
-    def __str__(self):
-        return  "Review: " + self.review
+   # def __str__(self):
+   #     return  "Review: " + self.review
+########
